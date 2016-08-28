9506184588bf55f3562a6dc0739a7b687c01f5a4
Sujin Lee <pasdesujin@gmail.com>
2016-08-28 19:36:56 -0400

    (feat) Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => (
+  <nav className="navbar navbar-inverse navbar-static-top">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">Sujin Lee</a>
+      </div>
+    </div>
+  </nav>
+)
+
+export default Header;

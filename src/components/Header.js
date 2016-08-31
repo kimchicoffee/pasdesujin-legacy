5b0443ec530e599f37235af78d548123310d01ed
Sujin Lee <pasdesujin@gmail.com>
2016-08-30 20:46:59 -0400

    (feat) Add email on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const Header = () => (
+const Header = () => {
+  const sendEmail = () => {
+    window.open('mailto:pasdesujin@gmail.com?subject=Job Inquiries','_self');
+  };
+
+  return(
   <nav className="navbar navbar-inverse navbar-static-top">
     <div className="container">
       <div className="navbar-header">
-        <a className="navbar-brand" href="#">Sujin Lee</a>
+        <a className="navbar-brand" href="/">Sujin Lee</a>
       </div>
+      <ul className="nav navbar-nav navbar-right">
+        <li>
+          <a href="#" onClick={sendEmail}>
+            Job Inquiries <i className="fa fa-envelope-o fa-lg" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
     </div>
   </nav>
-)
+  );
+}
 
 export default Header;

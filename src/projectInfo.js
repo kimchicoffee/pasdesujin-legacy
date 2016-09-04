6151dd0c9ed9e73c1137c1ec0f7a01084aa52e66
Sujin Lee <pasdesujin@gmail.com>
2016-09-03 23:50:37 -0400

    (feat) Update project info

--- src/projectInfo.js
+++ src/projectInfo.js
@@ -1,25 +1,33 @@
 const projectInfo = [
   {
-    name: "SpeakMirror",
-    image: "https://s11.postimg.org/k0qgaz9ib/Screen_Shot_2016_08_26_at_10_42_02_PM.png",
-    url:"https://speakmirror.us-west-2.elasticbeanstalk.com",
-    description: "Application to help improve communication with emotion recognition APIs",
-    stack: 'React, Redux, React-Router, Node/Express, AWS, Docker, Socketio',
+    name: 'SpeakMirror',
+    image: 'https://s11.postimg.org/k0qgaz9ib/Screen_Shot_2016_08_26_at_10_42_02_PM.png',
+    url: 'https://speakmirror.us-west-2.elasticbeanstalk.com',
+    description: 'Application to help improve communication with emotion recognition APIs',
+    stack: 'React, Redux, React-Router, Node/Express, AWS, Docker, Socketio, Stormpath, D3',
     role: 'Scrum Master / Full Stack Engineer'
   },
   {
-    name: "AirList",
-    image: "https://s21.postimg.org/w1ktbq27r/Screen_Shot_2016_08_30_at_3_29_19_PM.png",
-    url:"https://airlist.herokuapp.com",
-    description: "A web app allows user to lend and rent items to others",
+    name: 'AirList',
+    image: 'https://s21.postimg.org/w1ktbq27r/Screen_Shot_2016_08_30_at_3_29_19_PM.png',
+    url: 'https://airlist.herokuapp.com',
+    description: 'A web app allows user to lend and rent items to others',
     stack: 'Angular, Node/Express, Mongodb, Auth0',
     role: 'Full Stack Engineer'
   },
   {
-    name: "Pokemon List",
-    image: "https://s17.postimg.org/w14jpql3j/Screen_Shot_2016_08_29_at_11_30_17_PM.png",
-    url:"https://pokemonlist.herokuapp.com",
-    description: "A web app allows user to organize pokemon list",
+    name: 'Stack Salaries',
+    image: 'https://s15.postimg.org/ol96orctn/Screen_Shot_2016_08_30_at_11_02_23_PM.png',
+    url: 'https://salarystack.herokuapp.com/',
+    description: 'An app for software engineers to post and view anonymous salary info based on their stack and location',
+    stack: 'React, Redux, React-Router, Node/Express, Mongodb, Passport, D3',
+    role: 'Full Stack Engineer'
+  },
+  {
+    name: 'Pokemon List',
+    image: 'https://s17.postimg.org/w14jpql3j/Screen_Shot_2016_08_29_at_11_30_17_PM.png',
+    url:'https://pokemonlist.herokuapp.com',
+    description: 'A web app allows user to organize pokemon list',
     stack: 'Angular, Node/Express, Mongodb',
     role: 'Product Owner / Full Stack Engineer'
   }

5665748510f62ec1c24a5271e09d4930ab72b812
Sujin Lee <pasdesujin@gmail.com>
2016-09-13 11:03:38 -0400

    (chore) Change header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
 
   render() {
     return(
-      <nav className="navbar navbar-inverse navbar-static-top">
+      <nav className="navbar navbar-fixed-top navbar-inverse header">
         <div className="container">
           <div className="navbar-header">
             <a className="navbar-brand" href="/">Sujin Lee</a>

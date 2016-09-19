09f23dcbcda9ab43c38dbe469140d9d3d9ef5d7f
Sujin Lee <pasdesujin@gmail.com>
2016-09-18 22:36:58 -0700

    (chore) Change route to broswerHistory to display cleaner url

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/build'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/build/index.html'));
+});
 
 const PORT = process.env.PORT || 8000;
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, hashHistory } from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Router history={browserHistory}>{routes}</Router>,
   document.getElementById('root')
 );
